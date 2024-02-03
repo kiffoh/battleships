b7ec77732bdd2adea4ee95751c69635593e53c8d
Kiff <125292279+kiffoh@users.noreply.github.com>
2024-02-03 16:14:30 +0000

    Add the import styles.css

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import { buildGrid } from "./buildGrid";
-
+import "./styles.css"
 
 const playerGrid = document.querySelector("#playerGrid");
 const computerGrid = document.querySelector("#computerGrid");
