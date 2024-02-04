bc0c8bab024b793136bcddff944b94ce80bd1b1a
Kiff <125292279+kiffoh@users.noreply.github.com>
2024-02-04 12:11:11 +0000

    Create player and computer and populate with pre-set coordinates
    Starting with pre-set coordinates. System for allowing players to place their own coordinates will come later on

--- src/index.js
+++ src/index.js
@@ -1,8 +1,19 @@
 import { buildGrid } from "./buildGrid";
 import "./styles.css"
+import {Ship, Gameboard} from "./classCreator";
 
 const playerGrid = document.querySelector("#playerGrid");
 const computerGrid = document.querySelector("#computerGrid");
 
-buildGrid(playerGrid);
-buildGrid(computerGrid);
+
+
+const player = Gameboard();
+const computer = Gameboard();
+
+const startingCoordinates = [[0,0,0,2], [7,7,0,0], [2,4,2,2], [6,7,2,2], [0,1,6,6], [3,3,5,6], [3,3,8,8], [5,5,5,8], [7,7,7,7], [9,9,7,7]];
+
+player.positionShips(startingCoordinates);
+computer.positionShips(startingCoordinates);
+
+buildGrid(playerGrid, player.board);
+buildGrid(computerGrid, computer.board);
