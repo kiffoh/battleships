b755d374cf740695e16ff030d5b8190a62fec325
Kiff <125292279+kiffoh@users.noreply.github.com>
2024-02-04 16:58:50 +0000

    Create functions for toggleOverlay with switchTurn
    Maintains UX experience by keeping the overlay consistent.

--- src/gameLogic.js
+++ src/gameLogic.js
@@ -1,10 +1,42 @@
 import { goesFirst } from "./goesFirst";
 
 function gameLogic() {
-    let turn = goesFirst();
-    console.log(turn);
+    let firstTurn = goesFirst();
+    toggleOverlay(firstTurn, true);
+    /*
+    // Sets the conditions of the while loop
+    let playerShipsSunk = false;
+    let computerShipsSunk = false;
 
-    
+    while (!playerShipsSunk && !computerShipsSunk) {
+        let gridAttacked = document.querySelector(`${turn}Grid`);
+        for (let square in gridAttacked) {
+            square.addEventListener("click", () => {
+                square.textContent = "X";
+            })
+        }
+        turn = turn === "player" ? "computer" : "player";
+    }
+    */
+};
+
+function toggleOverlay(overlayID, showOverlay) {
+    const overlay = document.getElementById(overlayID + "Overlay");
+    if (showOverlay) {
+        overlay.style.display = "block";
+    } else {
+        overlay.style.display = "none";
+    }
+}
+
+function switchTurn(player) {
+    if (player === "player") {
+        toggleOverlay("computerGrid", false);
+        toggleOverlay("playerGrid", true);
+    } else {
+        toggleOverlay("computerGrid", true);
+        toggleOverlay("playerGrid", false);
+    }
 }
 
 export {gameLogic}
