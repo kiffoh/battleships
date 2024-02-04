6b0a6b86a436d0a3ce7a161e32778747e4c48e4f
Kiff <125292279+kiffoh@users.noreply.github.com>
2024-02-04 14:27:44 +0000

    Create function to determine first turn

---
+++ src/goesFirst.js
@@ -0,0 +1,18 @@
+function goesFirst() {
+    const players = ["player", "computer"];
+    let number = Math.random();
+    let index = Math.round(number);
+    let first = players[index];
+
+    const firstDiv = document.createElement("h3");
+    firstDiv.classList = "first-player";
+    if (first === "player") {
+        firstDiv.textContent = "PLAYER GOES FIRST";
+    } else {
+        firstDiv.textContent = "COMPUTER GOES FIRST";
+    }
+    document.body.appendChild(firstDiv);
+    return first;
+}
+
+export {goesFirst}
