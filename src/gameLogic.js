83f69edad256494566b4e0bb485e424bc3c3c5e1
Kiff <125292279+kiffoh@users.noreply.github.com>
2024-02-05 21:22:06 +0000

    Moved overlay functions and deleted irrelevant code

--- src/gameLogic.js
+++ src/gameLogic.js
@@ -1,42 +1,19 @@
 import { goesFirst } from "./goesFirst";
+import { toggleOverlay, switchTurn } from "./overlay";
 
-function gameLogic() {
-    let firstTurn = goesFirst();
-    toggleOverlay(firstTurn, true);
-    /*
-    // Sets the conditions of the while loop
-    let playerShipsSunk = false;
-    let computerShipsSunk = false;
+function gameLogic(player, computer) {
+    
+    console.log(player.allShipsSunk);
+    console.log(computer.allShipsSunk);
 
-    while (!playerShipsSunk && !computerShipsSunk) {
-        let gridAttacked = document.querySelector(`${turn}Grid`);
-        for (let square in gridAttacked) {
-            square.addEventListener("click", () => {
-                square.textContent = "X";
-            })
-        }
-        turn = turn === "player" ? "computer" : "player";
+    while (!player.allShipsSunk && !computer.allShipsSunk) {
+        console.log("FALSE");
+        player.allShipsSunk = true;
+        computer.allShipsSunk = true;
     }
-    */
+    console.log("TRUE");
 };
 
-function toggleOverlay(overlayID, showOverlay) {
-    const overlay = document.getElementById(overlayID + "Overlay");
-    if (showOverlay) {
-        overlay.style.display = "block";
-    } else {
-        overlay.style.display = "none";
-    }
-}
 
-function switchTurn(player) {
-    if (player === "player") {
-        toggleOverlay("computerGrid", false);
-        toggleOverlay("playerGrid", true);
-    } else {
-        toggleOverlay("computerGrid", true);
-        toggleOverlay("playerGrid", false);
-    }
-}
 
 export {gameLogic}
