ffa2531b6afd7213dd076f82d74d831abadefec1
Kiff <125292279+kiffoh@users.noreply.github.com>
2024-02-05 21:20:41 +0000

    Updated imports and refactored code for new classCreator.js functions

--- src/index.js
+++ src/index.js
@@ -1,30 +1,32 @@
-import { buildGrid } from "./buildGrid";
 import "./styles.css"
-import {Ship, Gameboard} from "./classCreator";
+import { Player } from "./classCreator";
 import { gameLogic } from "./gameLogic";
-import { addEventListeners } from "./addEventListeners";
+import { goesFirst } from "./goesFirst";
+import { toggleOverlay } from "./overlay";
 
 const playerGrid = document.querySelector("#playerGrid");
 const computerGrid = document.querySelector("#computerGrid");
 
+// Creating the player classes
+const player = Player();
+const computer = Player();
 
-
-const player = Gameboard();
-const computer = Gameboard();
-
+// Populating the player classes
 const startingCoordinates = [[0,0,0,2], [7,7,0,0], [2,4,2,2], [6,7,2,2], [0,1,6,6], [3,3,5,6], [3,3,8,8], [5,5,5,8], [7,7,7,7], [9,9,7,7]];
-
 player.positionShips(startingCoordinates);
 computer.positionShips(startingCoordinates);
 
-buildGrid(playerGrid, player.board);
-buildGrid(computerGrid, computer.board);
+player.buildGrid(playerGrid, true);
+computer.buildGrid(computerGrid, false);
 
 const startBtn = document.querySelector(".start-button");
 
 startBtn.addEventListener("click", () => {
-    addEventListeners(playerGrid);
-    addEventListeners(computerGrid);
-    gameLogic();
+    // Determines who goes first
+    let firstTurn = goesFirst();
+    toggleOverlay(firstTurn, true);
+
+    // gameLogic(player, computer);
+
     startBtn.disabled = true;
 });
