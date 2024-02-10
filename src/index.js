6a1671b120cd58b80160809ca3df46e0dbd569a3
Kiff <125292279+kiffoh@users.noreply.github.com>
2024-02-10 20:19:13 +0000

    Refactored functions and methods to match classCreator.js
    Additionally the name and opponent attribute have been added for easier tracking.

--- src/index.js
+++ src/index.js
@@ -1,32 +1,35 @@
 import "./styles.css"
 import { Player } from "./classCreator";
-import { gameLogic } from "./gameLogic";
 import { goesFirst } from "./goesFirst";
-import { toggleOverlay } from "./overlay";
-
-const playerGrid = document.querySelector("#playerGrid");
-const computerGrid = document.querySelector("#computerGrid");
 
 // Creating the player classes
 const player = Player();
+player.name = "player";
 const computer = Player();
+computer.name = "computer";
 
 // Populating the player classes
 const startingCoordinates = [[0,0,0,2], [7,7,0,0], [2,4,2,2], [6,7,2,2], [0,1,6,6], [3,3,5,6], [3,3,8,8], [5,5,5,8], [7,7,7,7], [9,9,7,7]];
 player.positionShips(startingCoordinates);
 computer.positionShips(startingCoordinates);
 
-player.buildGrid(playerGrid, true);
-computer.buildGrid(computerGrid, false);
+player.buildGrid(true);
+computer.buildGrid(false);
 
 const startBtn = document.querySelector(".start-button");
 
 startBtn.addEventListener("click", () => {
-    // Determines who goes first
-    let firstTurn = goesFirst();
-    toggleOverlay(firstTurn, true);
+    player.opponent = computer;
+    computer.opponent = player;
 
-    // gameLogic(player, computer);
+    // Determines who goes first
+    let turn = goesFirst([player, computer]);
+    turn.toggleOverlay(true);
+
+    //Starting the game
+    player.registerGridDivEventListener();
+    computer.registerGridDivEventListener();
 
     startBtn.disabled = true;
 });
+
