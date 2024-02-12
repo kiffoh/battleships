e7e8b5e101e082dc9944ceeee7ea37fcd5c51b0e
Kiff <125292279+kiffoh@users.noreply.github.com>
2024-02-12 22:53:08 +0000

    Changing computerGuess.js to be part of Player class if player is computer

--- src/computerGuess.js
+++ src/computerGuess.js
@@ -1,8 +1,13 @@
 function computerGuess(computerInstance, hit, prevGuess) {
-    if (hit) {
-        // Code to locate around the hit
-    }
-    else {
+    guessStatus = false;
 
+    while (!guessStatus) {
+        let yIndex = Math.floor(Math.random() * 10);
+        let xIndex = Math.floor(Math.random() * 10);
+
+        if (gameboard.board[yIndex][xIndex] === null && gameboard.missed[yIndex][xIndex] === null) {
+            
+        }
     }
+
 }
