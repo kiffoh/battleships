f5c742e490949b2f8d5dc453ac2b056986025786
Kiff <125292279+kiffoh@users.noreply.github.com>
2024-02-18 18:53:06 +0000

    Refactored functions names and position for better practices
    Changed the overlay function to a more intuitive name of showOverlay. Added the function to removeFirstPlayerText to only trigger once by adding it to the index file. Commented out the player.registerGridDivEventListener to work on the mode vs the computer.

--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
 import "./styles.css"
 import { Player } from "./classCreator";
-import { goesFirst } from "./goesFirst";
+import { goesFirst, removeFirstPlayerText } from "./goesFirst";
 
 // Creating the player classes
 const player = Player();
 player.name = "player";
 const computer = Player();
 computer.name = "computer";
+
+/* Maybe create a computer class - Makes a separation in the logic and makes it less confusing
+const player = Computer();
+player.name = "player";
+const computer = Computer();
+computer.name = "computer";
+*/
 
 // Populating the player classes
 const startingCoordinates = [[0,0,0,2], [7,7,0,0], [2,4,2,2], [6,7,2,2], [0,1,6,6], [3,3,5,6], [3,3,8,8], [5,5,5,8], [7,7,7,7], [9,9,7,7]];
@@ -24,11 +31,14 @@ startBtn.addEventListener("click", () => {
 
     // Determines who goes first
     let turn = goesFirst([player, computer]);
-    turn.toggleOverlay(true);
+    turn.showOverlay(true);
 
-    //Starting the game
-    player.registerGridDivEventListener();
+    // Starting the game
+    // For turn with computer I only register the event listeners on the computer squares
+    // player.registerGridDivEventListener();
     computer.registerGridDivEventListener();
+
+    removeFirstPlayerText(turn);
 
     startBtn.disabled = true;
 });
