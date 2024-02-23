ba7f240a2ce3094dd20aa2025c65259e729b9368
Kiff <125292279+kiffoh@users.noreply.github.com>
2024-02-23 22:29:50 +0000

    Add opponent choice at the start of game with playerOrComputer

--- src/index.js
+++ src/index.js
@@ -1,45 +1,54 @@
 import "./styles.css"
 import { Player } from "./classCreator";
-import { goesFirst, removeFirstPlayerText } from "./goesFirst";
+import { goesFirst, removeFirstPlayerText, playerOrComputer } from "./goesFirst";
 
-// Creating the player classes
-const player = Player();
-player.name = "player";
-const computer = Player();
-computer.name = "computer";
+async function initialiseGame() {
+    let opponent = await playerOrComputer();
+    console.log(opponent)
+    // Creating the player classes
+    const player1 = Player();
+    player1.name = "player";
+    const player2 = Player();
+    player2.name = `computer`;
+    console.log(opponent);
+    player1.buildHTML();
+    player2.buildHTML();
+    
+    player1.opponent = player2;
+    player2.opponent = player1;
 
-/* Maybe create a computer class - Makes a separation in the logic and makes it less confusing
-const player = Computer();
-player.name = "player";
-const computer = Computer();
-computer.name = "computer";
-*/
+    /* Maybe create a computer class - Makes a separation in the logic and makes it less confusing
+    const player = Computer();
+    player.name = "player";
+    const computer = Computer();
+    computer.name = "computer";
+    */
 
-// Populating the player classes
-const startingCoordinates = [[0,0,0,2], [7,7,0,0], [2,4,2,2], [6,7,2,2], [0,1,6,6], [3,3,5,6], [3,3,8,8], [5,5,5,8], [7,7,7,7], [9,9,7,7]];
-player.positionShips(startingCoordinates);
-computer.positionShips(startingCoordinates);
+    // Populating the player classes
+    const startingCoordinates = [[0,0,0,2], [7,7,0,0], [2,4,2,2], [6,7,2,2], [0,1,6,6], [3,3,5,6], [3,3,8,8], [5,5,5,8], [7,7,7,7], [9,9,7,7]];
+    player1.positionShips(startingCoordinates);
+    player2.positionShips(startingCoordinates);
 
-player.buildGrid(true);
-computer.buildGrid(false);
-
-const startBtn = document.querySelector(".start-button");
-
-startBtn.addEventListener("click", () => {
-    player.opponent = computer;
-    computer.opponent = player;
+    player1.buildGrid(true);
+    player2.buildGrid(false);
 
     // Determines who goes first
-    let turn = goesFirst([player, computer]);
+    let turn = goesFirst([player1, player2]);
     turn.showOverlay(true);
+    console.log(turn)
 
     // Starting the game
     // For turn with computer I only register the event listeners on the computer squares
-    // player.registerGridDivEventListener();
-    computer.registerGridDivEventListener();
+    if (player2.name != "computer") {
+        player1.registerGridDivEventListener();
+    }
+    player2.registerGridDivEventListener();
+
+    if (turn.name === "computer") {
+        player2.computerGuess();
+    }
 
     removeFirstPlayerText(turn);
+}
 
-    startBtn.disabled = true;
-});
-
+initialiseGame();
