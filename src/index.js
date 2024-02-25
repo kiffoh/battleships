29f2cd48e0cdfd2a4f0cee2d2f412146540f0e8b
Kiff <125292279+kiffoh@users.noreply.github.com>
2024-02-25 12:45:31 +0000

    Correct functions are applied based on the opponent selection

--- src/index.js
+++ src/index.js
@@ -30,41 +30,42 @@ async function initialiseGame() {
     player1.positionShips(startingCoordinates);
     player2.positionShips(startingCoordinates);
 
-    player1.buildGrid(true);
-    player2.buildGrid(false);
+    // player1.buildGrid(true);
+    // player2.buildGrid(false);
+
+    
+    // Starting the game
+    // For turn with computer I only register the event listeners on the computer squares
+    if (player2.name === "computer") {
+        // As playing computer the player's ships can be revealed
+        player1.buildGrid(true);
+        player2.buildGrid(false);
+
+        player2.generateComputerGuesses();
+        
+    } else {
+        // As playing on the same screen neither player can have ships revealed
+        player1.buildGrid(false);
+        player2.buildGrid(false);
+        
+        player1.registerGridDivEventListener();
+
+        
+    }
+    player2.registerGridDivEventListener();
 
     // Determines who goes first
     let turn = goesFirst([player1, player2]);
     turn.showOverlay(true);
     console.log(turn)
 
-    // Starting the game
-    // For turn with computer I only register the event listeners on the computer squares
-    if (player2.name === "computer") {
-        player2.generateComputerGuesses()
-
-        // As playing computer the player's ships can be revealed
-        /*
-        player1.buildGrid(true);
-        player2.buildGrid(false);
-        */
-    } else {
-        player1.registerGridDivEventListener();
-
-        // As playing on the same screen neither player can have ships revealed
-        /*
-        player1.buildGrid(false);
-        player2.buildGrid(false);
-        */
-    }
-    player2.registerGridDivEventListener();
-
     if (turn.name === "computer") {
         console.log("COMPUTER GUESSED FIRST")
         player2.computerGuess();
     }
 
-    removeFirstPlayerText(turn);
+    removeFirstPlayerText(player1.name);
+    removeFirstPlayerText(player2.name);
 }
 
 initialiseGame();
