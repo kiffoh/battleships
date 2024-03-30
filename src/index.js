d5d39dc6d1f8808314206d60ea7209ea95e8691d
Kiff <125292279+kiffoh@users.noreply.github.com>
2024-03-30 15:44:50 +0000

    Removed unnecessary comments

--- src/index.js
+++ src/index.js
@@ -18,22 +18,11 @@ async function initialiseGame() {
     player1.opponent = player2;
     player2.opponent = player1;
 
-    /* Maybe create a computer class - Makes a separation in the logic and makes it less confusing
-    const player = Computer();
-    player.name = "player";
-    const computer = Computer();
-    computer.name = "computer";
-    */
-
     // Populating the player classes
     const startingCoordinates = [[0,0,0,2], [7,7,0,0], [2,4,2,2], [6,7,2,2], [0,1,6,6], [3,3,5,6], [3,3,8,8], [5,5,5,8], [7,7,7,7], [9,9,7,7]];
     player1.positionShips(startingCoordinates);
     player2.positionShips(startingCoordinates);
 
-    // player1.buildGrid(true);
-    // player2.buildGrid(false);
-
-    
     // Starting the game
     // For turn with computer I only register the event listeners on the computer squares
     if (player2.name === "computer") {
@@ -48,9 +37,8 @@ async function initialiseGame() {
         player1.buildGrid(false);
         player2.buildGrid(false);
         
+        // Register event listeners on both grids
         player1.registerGridDivEventListener();
-
-        
     }
     player2.registerGridDivEventListener();
 
