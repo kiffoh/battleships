3af8be93a08b3538e1e35ff665cec25569757085
Kiff <125292279+kiffoh@users.noreply.github.com>
2024-04-08 14:21:25 +0100

    Created logic for confirmation buttons
    Created logic for when confirmation buttons are pressed in single-player and muti-player. Functions changeOverlaysTo and hideShipsContainer were required for the UX of ship position stage

--- src/goesFirst.js
+++ src/goesFirst.js
@@ -78,4 +78,92 @@ function playerOrComputer() {
 
 };
 
-export { goesFirst, removeFirstPlayerText, playerOrComputer }
+async function handleConfirmBtnClick(player1, player2) {
+    if (player2.name === "computer") {
+        await singlePlayerConfirmBtn(player1);
+    } else {
+        await multiPlayerConfirmBtn(player1, player2);
+    }
+}
+
+function singlePlayerConfirmBtn(player1) {
+    const player1ConfirmBtn = document.querySelector(".player1.confirm-btn");
+    let resolvePromise;
+
+    const moveOnPromise = new Promise(resolve => {
+        resolvePromise = resolve;
+        player1ConfirmBtn.onclick = () => {
+            player1.removeShipsAndButtons();
+
+            resolve();
+        }
+    })
+    
+    return moveOnPromise;
+}
+
+function multiPlayerConfirmBtn(player1, player2) {
+    // Change the overlay colour for ship placement
+    changeOverlaysTo("blue");
+    hideShipsContainer(true, player2.name)
+
+    const player1ConfirmBtn = document.querySelector(".player1.confirm-btn");
+
+    player1ConfirmBtn.onclick = () => {
+        player1.showOverlay(true);
+        player2.showOverlay(false);
+
+        hideShipsContainer(false, player2.name)
+        hideShipsContainer(true, player1.name)
+    }
+
+    const player2ConfirmBtn = document.querySelector(".player2.confirm-btn");
+    let resolvePromise; // Declare a variable to store the resolve function
+    
+    const moveOnPromise = new Promise(resolve => {
+        // Assign the resolve function to the variable
+        resolvePromise = resolve;
+        
+        player2ConfirmBtn.onclick = () => {
+            player1.removeShipsAndButtons();
+            player2.removeShipsAndButtons();
+            player1.showOverlay(false);
+    
+            player1.hideShips();
+            player2.hideShips();
+            // Revert overlay colour back to original
+            changeOverlaysTo("partially-transparent");
+            
+            // Resolve the promise when the player 2 confirmation button is clicked
+            resolve();
+        }
+    });
+    
+    // Return the promise
+    return moveOnPromise;
+}
+
+function changeOverlaysTo(colour) {
+    const playerOverlays = [document.querySelector(`#player1GridOverlay`), document.querySelector(`#player2GridOverlay`)];
+    if (colour === "blue") {
+        playerOverlays.forEach(overlay => {
+            overlay.style.backgroundColor = "rgb(73, 110, 235)";
+        });
+    } else if (colour === "partially-transparent") {
+        playerOverlays.forEach(overlay => {
+            overlay.style.backgroundColor = "rgba(0, 0, 0, 0.5)";
+        });
+    }
+}
+
+function hideShipsContainer(hide, playerName) {
+    const shipsContainer = document.querySelector(`.${playerName}-grid-container > .ships-container`);
+    if (hide) {
+        shipsContainer.style.display = "none";
+    } else {
+        shipsContainer.style.display = "flex";
+    }
+
+}
+
+export { goesFirst, removeFirstPlayerText, playerOrComputer, handleConfirmBtnClick }
