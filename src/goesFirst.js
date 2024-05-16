87860e0376b19a7ff8509f2233571ba585c5cb9a
Kiff <125292279+kiffoh@users.noreply.github.com>
2024-05-16 09:25:18 +0100

    Logic for selection of player moniker's
    Additionally updated first to moniker as the text in the div is user facing. Added functionality to allow the user to go back to player selection stage before confirming moniker's to switch the opponent

--- src/goesFirst.js
+++ src/goesFirst.js
@@ -1,7 +1,7 @@
 function goesFirst(players) {
     let number = Math.random();
     let index = Math.round(number);
-    let first = players[index].name;
+    let first = players[index].moniker;
 
     const turnDiv = document.querySelector(".turn-text");
     turnDiv.textContent = `${first.toUpperCase()} GOES FIRST`;
@@ -45,7 +45,28 @@ function playerOrComputer() {
                     <button class="computer-btn">COMPUTER</button>
                 </div>
             </div>
+            <div class="confirm-player-or-computer-btn-container">
+                <button class="confirm-player-or-computer-btn">CONFIRM</button>
+            </div>
             <div class="player-selection-overlay"></div>            
+        </div>
+        <div class="moniker-selection">
+            <div class="player1-moniker-container">
+                <input type="text" class="player1-moniker-text" placeholder="PLAYER1" value="PLAYER1" size="10" maxlength="15">
+            </div>
+            <h3>VS</h3>
+            <div class="player2-moniker-container">
+                <input type="text" class="player2-moniker-text" placeholder="PLAYER2" value="PLAYER2" size="10" maxlength="15">
+            </div>
+            <h3 class="player2-name-is-computer">COMPUTER</h3>
+            <div class="moniker-btns-container">
+                <div class="confirm-moniker-btn-container">
+                    <button class="confirm-moniker-btn">CONFIRM</button>
+                </div>
+                <div class="back-btn-container">
+                    <button class="back-btn">BACK</button>
+                </div>
+            </div>
         </div>
         <div class="game-rules-overlay" id="gameRulesOverlay">
             <h3 class="game-rules-title">GAME RULES</h3>
@@ -62,30 +83,104 @@ function playerOrComputer() {
     // Append the player-selection div to the grid-overlay div
     gridOverlay.appendChild(centeredContainerDiv);
 
+    // Variable to hold the chosen player
+    let chosenPlayer2 = null;
+
+    // Elements obtained to display correct div depending on player selection stage
+    const playerSelectionDiv = document.querySelector(".player-selection");
+    const monikerSelectionDiv = document.querySelector(".moniker-selection");
+
+    // Player2 selection buttons
     const player2Btn = document.querySelector(".player-2-btn");
     const computerBtn = document.querySelector(".computer-btn");
 
-    // Implement promise for first one to be resolved.
-    const player2Promise = new Promise((resolve, reject) => {
-        player2Btn.addEventListener("click", () => {
-            console.log("PLAYER BUTTON PRESSED")
-            gridOverlay.innerHTML = "";
-            gridOverlay.style.display = "none";
-            resolve("player");
-        })
-    })
-    
-    const computerPromise = new Promise((resolve, reject) => {
-        computerBtn.addEventListener("click", () => {
-            console.log("COMPUTER BUTTON PRESSED")
-            gridOverlay.innerHTML = "";
-            gridOverlay.style.display = "none";
-            resolve("computer");
-        })
-    })
-    
-    return Promise.any([player2Promise, computerPromise])
+    // Obtain moniker text input elements
+    const player1MonikerText = document.querySelector(".player1-moniker-text");
+    const player2MonikerText = document.querySelector(".player2-moniker-text");
+    const player2NameIsComputer = document.querySelector(".player2-name-is-computer");
 
+    // Variables to hold moniker text values
+    let player1MonikerValue = "PLAYER1";
+    let player2MonikerValue = "PLAYER2";
+    
+    // Functions to update and display the selected player
+    player2Btn.addEventListener("click", () => {
+        chosenPlayer2 = "player";
+        player2Btn.classList.add("highlighted", "green");
+        computerBtn.classList.remove("highlighted", "green");
+    })
+
+    computerBtn.addEventListener("click", () => {
+        chosenPlayer2 = "computer";
+        computerBtn.classList.add("highlighted", "green");
+        player2Btn.classList.remove("highlighted", "green");
+    })
+
+    // Progresses player to input a name (moniker)
+    const chosenPlayerConfirmBtn = document.querySelector(".confirm-player-or-computer-btn");
+    chosenPlayerConfirmBtn.addEventListener("click", () => {
+        if (chosenPlayer2) {
+            playerSelectionDiv.style.display = "none";
+            monikerSelectionDiv.style.display = "flex";
+
+            if (chosenPlayer2 === "computer") {
+                // Make internal equal computer
+                player2MonikerText.style.display = "none";
+                player2MonikerValue = "COMPUTER";
+                player2NameIsComputer.style.display = "block";
+            } else {
+                player2NameIsComputer.style.display = "none";
+                player2MonikerText.style.display = "block";
+                player2MonikerValue = player2MonikerText.value;
+            }
+        } else {
+            chosenPlayerConfirmBtn.classList.add("denied");
+
+            chosenPlayerConfirmBtn.addEventListener("animationend", () => {
+                chosenPlayerConfirmBtn.classList.remove("denied");
+            })
+        }
+    })
+
+    // Event listners to update the stored moniker value when the input changes
+    player1MonikerText.addEventListener("input", (event) => {
+        player1MonikerValue = event.target.value;
+    })
+
+    player2MonikerText.addEventListener("input", (event) => {
+        player2MonikerValue = event.target.value;
+    })
+
+    // Allows user to re-select a different opponent after leaving player selection screen
+    const backBtn = document.querySelector(".back-btn");
+    backBtn.addEventListener("click", () => {
+        monikerSelectionDiv.style.display = "none";
+        playerSelectionDiv.style.display = "flex";
+    })
+
+    // Promise moves the player to the next stage of the game
+    const monikerConfirmBtn = document.querySelector(".confirm-moniker-btn");
+    const monikerConfirmPromise = new Promise(resolve => {
+        monikerConfirmBtn.addEventListener("click", () => {
+            if (player1MonikerValue === "") {
+                player1MonikerValue = "PLAYER1";
+            }
+            if (player2MonikerValue === "") {
+                player2MonikerValue = chosenPlayer2 === "computer" ? "COMPUTER" : "PLAYER2";
+            }
+
+            if (player1MonikerValue === player2MonikerValue) {
+                player1MonikerValue = player1MonikerValue + "1"
+                player2MonikerValue = player2MonikerValue + "2"
+            }
+
+            resolve({ chosenPlayer2, player1MonikerValue, player2MonikerValue });
+            gridOverlay.innerHTML = "";
+            gridOverlay.style.display = "none";
+        });
+    });
+
+    return monikerConfirmPromise;
 };
 
 function dullTitleText(dull) {
